5d961017baaf6532daa938096dce10496872bd52
Patrick Mallee <patmallee@gmail.com>
2016-02-08 23:48:03 +0000

    Add index, show, new, and edit actions to kitten controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,13 +1,29 @@
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
   end
 
   def new
+    @kitten = Kitten.new
+  end
+  
+  def create
+    #to be filled
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
+  end
+  
+  def update
+    #to be filled
+  end
+  
+  def destroy
+    #to be filled
   end
 end
